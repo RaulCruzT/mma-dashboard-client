12ad162147d42495e7eff653ada701cfea4346ee
RaulCruzT <cruztorresraul@gmail.com>
2023-11-03 22:17:41 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,33 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node';
 
 export default defineConfig({
   plugins: [react()],
+  base: '', 
   optimizeDeps: {
-      esbuildOptions: {
-          define: {
-              global: 'globalThis'
-          },
-          plugins: [
-              NodeGlobalsPolyfillPlugin({
-                  buffer: true
-              })
-          ]
-      }
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true, 
+          process: true,
+        }), 
+        NodeModulesPolyfillPlugin() 
+      ]
+    }
+  }, 
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill()
+      ]
+    }
   }
 });
